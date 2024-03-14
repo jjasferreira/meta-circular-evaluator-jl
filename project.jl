a812abb80637c9485b52489d6034535d68d53c53
Francisco Carvalho <franciscojcarvalho@tecnico.ulisboa.pt>
2024-03-14 12:29:34 +0000

    feat: Added "repl" loop

---
+++ project.jl
@@ -0,0 +1,8 @@
+function metajulia_repl()
+    while(true) #? Should it have an exit condition?
+        print(">> ")
+        line = readline()
+        #TODO: Call parser
+        #TODO: Call evaluator 
+    end
+end
