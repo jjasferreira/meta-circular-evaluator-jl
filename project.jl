d71a7b0e00517056675923cd6a0a70e98163e5f9
José João Ferreira <jjasferreira@gmail.com>
2024-03-17 01:40:15 +0000

    Develop `metajulia_repl()` logic

--- project.jl
+++ project.jl
@@ -1,8 +1,28 @@
+
+
+
 function metajulia_repl()
-    while(true) #? Should it have an exit condition?
+    println("MetaJulia REPL. Type \"exit\" to quit.")
+    while true
+        # Read user input
         print(">> ")
-        line = readline()
-        #TODO: Call parser
-        #TODO: Call evaluator 
+        input = readline()
+        # Check for the exit command
+        if input == "exit"
+            println("Exiting MetaJulia REPL.")
+            break
+        end
+        # Parse the input into an AST
+        ast = Meta.parse(input)
+        # Evaluate the AST and print the result
+        if isa(ast, Expr) || isa(ast, Number) || isa(ast, String) || isa(ast, Symbol)
+            # result = evaluate(ast)
+            println(ast)
+        # Unsupported AST types
+        else
+            error("Unsupported AST node type: $(typeof(ast))")
+        end
     end
 end
+
+
