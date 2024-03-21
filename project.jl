caa599e114fa3840770c806fe49b947fe2dfafe5
Francisco Carvalho <franciscojcarvalho@tecnico.ulisboa.pt>
2024-03-21 19:59:36 +0000

    feat: implemented assignment

--- project.jl
+++ project.jl
@@ -1,11 +1,51 @@
+# ------------------------------------------------------------------------------
+# Symbol Table and it's aux functions
+#
+# Notas do prof: LISTAS DE LISTAS COM OS BOUNDS!
+#                DO JULIA PADRÃO SO CONSEGUIMOS USAR AS PRIMITIVAS
+#
+# ------------------------------------------------------------------------------
+
+globalScope = Dict{String, Any}(); # global symbol table 
+
+"""Creates a new environment inside the existing one"""
+function newEnvironment(super_env::Dict) :: Dict
+    environment = Dict{String, Any}();
+    environment["#"] = super_env #FIXME: By ref?
+    return environment
+end
+
+"""
+Binds a symbol to a name (variable) in an environment
+If the variable is already defined its value will be replaced
+"""
+function addVariableToEnv(var_name::String, var::Any, env::Dict)
+    env[var_name]=var
+end
+
+"""Retrieves a the symbol of a variable from the environment"""
+function getVariableValue(var_name::String, env::Dict) # Nullable
+    # this will be a recursive function
+    if(haskey(env, var_name))
+        return env[var_name]
+    end
+    if(!haskey(env, "#")) # global scope
+        return nothing
+    end
+    return getVariableValue(var_name, env["#"]) # scope before
+end
 
 
-
-function evaluate(ast)
-    if isa(ast, Number) || isa(ast, String) # Literals
+function evaluate(ast, env::Dict=globalScope)
+    # Literals
+    if isa(ast, Number) || isa(ast, String)
         return ast
-    elseif isa(ast, Expr)                   # Expressions
-        if ast.head == :call                    # Function calls
+        # Expressions
+    elseif isa(ast, Expr)
+        println("[rm later] AST head type: $(ast.head)") # rm line
+        println("[rm later] AST args: $(ast.args)") # rm line
+        # Function calls
+        if ast.head == :call
             op = ast.args[1]
             args = map(evaluate, ast.args[2:end])
             if op == :+
@@ -23,8 +63,12 @@ function evaluate(ast)
             else
                 error("Unsupported operation: $op")
             end
-
-        elseif ast.head == :||                  # Logical OR    
+            # Logical operators
+        elseif ast.head == :(=)
+            value = evaluate(ast.args[2], env)
+            addVariableToEnv(string(ast.args[1]), value, env)
+            return value
+        elseif ast.head == :||
             return evaluate(ast.args[1]) || evaluate(ast.args[2])
         elseif ast.head == :&&                  # Logical AND
             return evaluate(ast.args[1]) && evaluate(ast.args[2])
@@ -43,10 +87,17 @@ function evaluate(ast)
                 val = evaluate(i)
             end
             return val
-
-        else                                # Unsupported expressions
+        elseif ast.head == :let
+            #let evaluate(ast.args[1]) # let evaluate(:(x=1))
+            #    return evaluate(ast.args[2]) 
+            #end
+        else
             error("Unsupported expression type: $(ast.head)")
         end
+    else
+        #! MARTELADA - Não sabemos bem o que isto é
+        value = getVariableValue(string(ast), env)
+        return  isnothing(value) ? error("Symbol is not defined") : value
     end
 end
 
@@ -85,7 +136,8 @@ function metajulia_repl()
         #println("[rm] arg 2 2: $(ast.args[2].args[2])")
 
         if isa(ast, Expr) || isa(ast, Number) || isa(ast, String) || isa(ast, Symbol)
-            result = evaluate(ast)  # Evaluate the AST and print the result
+            result = evaluate(ast, globalScope)
+            #?                 ^ should global? ^
             printlnr(result)
         else                        # Unsupported AST types
             error("Unsupported AST node type: $(typeof(ast))")
